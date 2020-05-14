b6457a8b03daf6068b1954dc6d5c150d49fda7b1
jaymaina <jaymaina5@gmail.com>
2020-05-15 01:14:51 +0300

    added and deleted some migrations

--- db/migrate/20200513215557_add_columns_to_user_table.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToUserTable < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :username, :string
-    add_column :users, :full_name, :string
-  end
-end
--- db/migrate/20200514203411_add_user_refs_to_tweets.rb
+++ db/migrate/20200514203411_add_user_refs_to_tweets.rb
@@ -1,4 +1,4 @@
-class AddUserRefsToTweet < ActiveRecord::Migration[5.2]
+class AddUserRefsToTweets < ActiveRecord::Migration[5.2]
   def change
     add_reference :tweets, :author, references: :users, index: true
     add_foreign_key :tweets, :users, column: :author_id
