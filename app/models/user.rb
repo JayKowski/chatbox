37e2800f82619f08fb550c0a6b137451ef024b34
jaymaina <jaymaina5@gmail.com>
2020-05-15 01:09:09 +0300

    changed some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    #   devise :database_authenticatable, :registerable,
-    #          :recoverable, :rememberable, :validatable
-
     #validations
 
     validates :username, presence: true, length: { minimum: 3, maximum: 15 }
